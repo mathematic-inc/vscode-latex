069860eb99149d2018488a5f361c8b58f7c14bd7
Randolf Jung <randolf@mathematic.io>
2026-08-30 03:45:03 -0700

    fix: handle multiline executable locator output

---
+++ src/executable.test.mjs
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2021 Mathematic Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import assert from "node:assert/strict";
+import { chmodSync, mkdtempSync, rmSync, writeFileSync } from "node:fs";
+import { tmpdir } from "node:os";
+import { join } from "node:path";
+import test from "node:test";
+
+import { findExecutable } from "./executable.ts";
+
+test("returns the first executable from multiline locator output", (context) => {
+  const directory = mkdtempSync(join(tmpdir(), "vscode-latex-"));
+  context.after(() => rmSync(directory, { recursive: true }));
+
+  const first = join(directory, "first");
+  const second = join(directory, "second");
+  for (const file of [first, second]) {
+    writeFileSync(file, "");
+    chmodSync(file, 0o755);
+  }
+
+  assert.equal(findExecutable(`${first}\r\n${second}\r\n`), first);
+});
